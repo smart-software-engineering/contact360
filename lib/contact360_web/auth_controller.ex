aedc678286ca04e1d97a4ce6b94565880909faf0
Rico Metzger <rico.metzger@gmail.com>
2024-06-20 05:26:53 +0300

    More infos

--- lib/contact360_web/auth_controller.ex
+++ lib/contact360_web/auth_controller.ex
@@ -23,25 +23,36 @@ defmodule Contact360Web.AuthController do
   end
 
   def callback(%{assigns: %{ueberauth_auth: auth}} = conn, _params) do
-    IO.inspect(auth, label: "Auth Response")
     user = extract_user_info(auth)
-    token_name = String.to_existing_atom("#{auth.provider}_user")
+    session_user_name = String.to_existing_atom("#{auth.provider}_user")
 
     conn
-    |> put_flash(:info, "Successfully authenticated with #{auth.provider}.")
-    |> put_session(token_name, user)
-    |> configure_session(renew: true)
-    |> redirect(to: "/")
+    |> login_user_into_auth(auth.provider, user, session_user_name)
+    |> redirect(to: "/") # TODO: keep track of the last visited page
   end
 
   defp extract_user_info(%Ueberauth.Auth{} = auth) do
+    IO.inspect(auth: "Bexio Auth Info")
+
     %{
       firstname: auth.info.first_name,
       lastname: auth.info.last_name,
       email: auth.info.email,
       token: auth.credentials.token,
       token_type: auth.credentials.token_type,
-      expires_at: auth.credentials.expires_at
+      expires_at: auth.credentials.expires_at,
+      refresh_token: auth.credentials.refresh_token,
+      scopes: auth.credentials.scopes,
+      locale: to_locale(auth.extra.locale)
     }
+  end
+
+  defp to_locale("de_CH"), do: :de_CH
+  defp to_locale("fr_CH"), do: :fr_CH
+
+  defp login_user_into_auth(conn, :bexio, user, session_user_name) do
+    conn
+    |> configure_session(renew: true)
+    |> put_session(session_user_name, user)
   end
 end
