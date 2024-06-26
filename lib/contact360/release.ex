5822b3aaa6f9074a95c3328d1d6d57c514765ff5
Rico Metzger <rico.metzger@gmail.com>
2024-06-26 23:32:24 +0200

    Release prepared

--- lib/contact360/release.ex
+++ lib/contact360/release.ex
@@ -10,12 +10,28 @@ defmodule Contact360.Release do
 
     for repo <- repos() do
       {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :up, all: true))
+      migrate_tenant_schemas(repo)
     end
+  end
+
+  defp migrate_tenant_schemas(repo) do
+    path = Application.app_dir(@app, "priv/repo/tenant_migrations")
+
+    Triplex.all()
+    |> Enum.each(&Ecto.Migrator.run(repo, path, :up, all: true, prefix: &1.prefix))
+    |> IO.inspect(label: "Migrate tenant schemas")
   end
 
   def rollback(repo, version) do
     load_app()
     {:ok, _, _} = Ecto.Migrator.with_repo(repo, &Ecto.Migrator.run(&1, :down, to: version))
+    # TODO what is the result??
+    Triplex.all()
+    |> Ecto.Migrator.with_repo(
+      repo,
+      &Ecto.Migrator.run(&1, :down, to: version, prefix: &1.prefix)
+    )
+    |> IO.inspect(label: "Rollback tenant schemas")
   end
 
   defp repos do
